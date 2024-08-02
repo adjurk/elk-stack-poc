f875aba851e6835aef92083f8fd0d0f54d384190
Adam Jurkiewicz <a.jurkiewicz5@gmail.com>
2024-08-02 20:45:22 +0200

    Add VPC config with private/public subnets

--- infra/828908252101/eu-central-1/provider.tf
+++ infra/828908252101/eu-central-1/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.61.0"
     }
   }
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region  = "eu-central-1"
   profile = "elk-stack-poc"
 }
